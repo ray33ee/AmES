89748dd1ec44b764c791f3a4988507860962905d
ray33ee <30669752+ray33ee@users.noreply.github.com>
2021-01-11 16:12:24 +0000

    Version 0.1.1 - Various changes, see changelog
    Signed-off-by: ray33ee <30669752+ray33ee@users.noreply.github.com>

---
+++ src/ames_cipher/mod.rs
@@ -0,0 +1,353 @@
+
+pub mod ames {
+    use galois_2p8::{IrreducablePolynomial, PrimitivePolynomialField};
+    use galois_2p8::Field;
+
+    use std::fmt;
+
+    use rand_chacha::{ChaChaRng, rand_core::SeedableRng};
+
+    use rand::seq::SliceRandom;
+    use rand::distributions::{Uniform, Distribution};
+    use rand::Rng;
+
+    use cipher::block::{BlockCipher, NewBlockCipher, Key};
+    use cipher::consts::{U32, U16, U1};
+    use cipher::block::Block;
+
+    use num_derive::{ToPrimitive, FromPrimitive};
+    use num_traits::FromPrimitive;
+
+    const STATE_SIZE: usize = 16;
+    const ROW_SIZE: usize = 4;
+
+    //Number of possible actions. Currently 4 (XOR, Scramble, Linear and SBox)
+    // If more actions are added, this number must be updated accordingly
+    const ACTION_COUNT: u32 = 4;
+
+    #[derive(Debug, Clone, Copy, ToPrimitive, FromPrimitive)]
+    enum Action {
+        XOR,
+        Scramble,
+        Linear,
+        SBox
+    }
+
+    trait SBox {
+        fn forward(&self, byte: u8) -> u8;
+
+        fn reverse(&self, byte: u8) -> u8;
+    }
+
+    struct ArraySbox {
+        _forward: Vec<u8>,
+        _reverse: Vec<u8>
+    }
+
+    impl ArraySbox {
+        fn new(list: Vec<u8>) -> Self {
+            let mut reverse = vec![0; 256];
+
+            for (i, map_element) in list.iter().enumerate() {
+                reverse[*map_element as usize] = i as u8;
+            }
+
+            ArraySbox {
+                _forward: list,
+                _reverse: reverse
+            }
+        }
+    }
+
+    impl SBox for ArraySbox {
+
+        fn forward(&self, byte: u8) -> u8 {
+            self._forward[byte as usize]
+        }
+
+        fn reverse(&self, byte: u8) -> u8 {
+            self._reverse[byte as usize]
+        }
+
+    }
+
+    struct State<'a, 'b> {
+        _field: & 'a PrimitivePolynomialField,
+        _array: & 'b mut [u8]
+    }
+
+    impl fmt::Debug for State<'_, '_> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.debug_struct("State")
+                .field("_array", &self._array)
+                .field("_field", &IrreducablePolynomial::Poly84310)
+                .finish()
+        }
+    }
+
+    impl<'a, 'b> State<'a, 'b> {
+        fn new(field: & 'a PrimitivePolynomialField, array: & 'b mut [u8]) -> Self {
+            State {
+                _field: field,
+                _array: array
+            }
+        }
+
+        fn xor(& mut self, key: & [u8]) {
+            for (state_byte, key_byte) in self._array.iter_mut().zip(key.iter()) {
+                *state_byte = self._field.add(*key_byte, *state_byte);
+            }
+        }
+
+        fn scramble(& mut self, permutation: & [u8; STATE_SIZE]) {
+            let mut tmp = [0; STATE_SIZE];
+
+            for (tmp_byte, permuter) in tmp.iter_mut().zip(permutation.iter()) {
+                *tmp_byte = self._array[*permuter as usize]
+            }
+
+            self._array.clone_from_slice(&tmp);
+        }
+
+        fn linear(& mut self, polynomial: & [u8; ROW_SIZE]) {
+            for i in 0..ROW_SIZE {
+                let row = & mut self._array[i*ROW_SIZE..i*ROW_SIZE+ROW_SIZE];
+
+                let mut new_row = [0u8; ROW_SIZE];
+
+                for (j, new_row_element) in new_row.iter_mut().enumerate() {
+
+                    for (k, row_element) in row.iter().enumerate() {
+                        //Perform some dirty modular arithmetic to shift each row
+                        let polynomial_element = polynomial[(k + 7 - j) % ROW_SIZE];
+                        //Perform matrix multiplication by multiplying each element and adding them up
+                        *new_row_element = self._field.add(*new_row_element, self._field.mult(polynomial_element, *row_element))
+                    }
+                }
+
+                row.clone_from_slice(&new_row);
+            }
+        }
+
+        fn remap<F>(& mut self, sbox: &F)
+            where F: Fn(u8) -> u8
+        {
+            for byte in self._array.iter_mut() {
+                *byte = sbox(*byte)
+            }
+        }
+
+        fn transform<F>(& mut self,
+                        sequence: & [Action],
+                        key: & [u8],
+                        polynomial: & [u8; ROW_SIZE],
+                        permutation: & [u8; STATE_SIZE],
+                        sbox: F)
+            where F: Fn(u8) -> u8
+        {
+
+            for action in sequence.iter() {
+                match action {
+                    Action::XOR => {
+                        self.xor(key);
+                    },
+                    Action::Linear => {
+                        self.linear(polynomial);
+                    },
+                    Action::SBox => {
+                        self.remap(&sbox);
+                    },
+                    Action::Scramble => {
+                        self.scramble(permutation);
+                    }
+                }
+            }
+        }
+
+    }
+
+    fn invert_map(map: & [u8]) -> [u8; STATE_SIZE] {
+        let mut inverted = [0; STATE_SIZE];
+
+        for (i, map_element) in map.iter().enumerate() {
+            inverted[*map_element as usize] = i as u8;
+        }
+
+        inverted
+    }
+
+    // Returns the determinant of the 4x4 circulant matrix generated by 'polynomial'
+    fn determinant(field: & PrimitivePolynomialField, polynomial: & [u8; ROW_SIZE]) -> u8 {
+
+        let alpha = polynomial[0];
+        let beta = polynomial[1];
+        let gamma = polynomial[2];
+        let delta = polynomial[3];
+
+        field.mult(field.mult(alpha, alpha), field.mult(alpha, alpha)) ^ field.mult(field.mult(alpha, beta), field.mult(beta, gamma)) ^  field.mult(field.mult(alpha, gamma), field.mult(delta, delta)) ^
+
+            field.mult(field.mult(beta, delta), field.mult(beta, delta)) ^   field.mult(field.mult(beta, beta), field.mult(gamma, alpha)) ^  field.mult(field.mult(beta, gamma), field.mult(alpha, beta)) ^
+
+            field.mult(field.mult(gamma, delta), field.mult(delta, alpha)) ^ field.mult(field.mult(gamma, alpha), field.mult(beta, beta)) ^  field.mult(field.mult(gamma, gamma), field.mult(gamma, gamma)) ^
+
+            field.mult(field.mult(delta, delta), field.mult(alpha, gamma)) ^ field.mult(field.mult(delta, alpha), field.mult(gamma, delta)) ^field.mult(field.mult(delta, beta), field.mult(delta, beta)) ^
+
+            field.mult(field.mult(alpha, alpha), field.mult(beta, delta)) ^  field.mult(field.mult(alpha, beta), field.mult(delta, alpha)) ^ field.mult(field.mult(alpha, gamma), field.mult(alpha, gamma)) ^
+
+            field.mult(field.mult(beta, delta), field.mult(alpha, alpha)) ^  field.mult(field.mult(beta, beta), field.mult(beta, beta)) ^    field.mult(field.mult(beta, gamma), field.mult(gamma, delta)) ^
+
+            field.mult(field.mult(gamma, delta), field.mult(beta, gamma)) ^  field.mult(field.mult(gamma, alpha), field.mult(gamma, alpha)) ^field.mult(field.mult(gamma, gamma), field.mult(delta, beta)) ^
+
+            field.mult(field.mult(delta, delta), field.mult(delta, delta)) ^ field.mult(field.mult(delta, alpha), field.mult(alpha, beta)) ^ field.mult(field.mult(delta, beta), field.mult(gamma, gamma))
+    }
+
+    // Use part of the hard coded 4x4 matrix inversion algorithm to get the inverted polynomial by inverting the circulant matrix generated by the polynomial
+    fn invert_polynomial(field: & PrimitivePolynomialField, polynomial: & [u8; ROW_SIZE]) -> [u8; ROW_SIZE] {
+
+        let mut result = [0; 4];
+
+        let alpha = polynomial[0];
+        let beta = polynomial[1];
+        let gamma = polynomial[2];
+        let delta = polynomial[3];
+
+        let determinant = determinant(field, polynomial);
+
+        result[2] = field.div(field.mult(field.mult(alpha, alpha), alpha) ^field.mult(field.mult(beta, beta), gamma) ^field.mult(field.mult(gamma, delta), delta) ^
+                                  field.mult(field.mult(alpha, beta), delta) ^field.mult(field.mult(beta, delta), alpha) ^field.mult(field.mult(gamma, alpha), gamma), determinant);
+
+        result[3] = field.div(field.mult(field.mult(beta, beta), delta) ^field.mult(field.mult(gamma, delta), alpha) ^field.mult(field.mult(delta, alpha), gamma) ^
+                                  field.mult(field.mult(beta, alpha), alpha) ^field.mult(field.mult(gamma, beta), gamma) ^field.mult(field.mult(delta, delta), delta), determinant);
+
+        result[0] = field.div(field.mult(field.mult(beta, beta), alpha) ^field.mult(field.mult(gamma, gamma), gamma) ^field.mult(field.mult(delta, alpha), delta) ^
+                                  field.mult(field.mult(beta, gamma), delta) ^field.mult(field.mult(gamma, alpha), alpha) ^field.mult(field.mult(delta, beta), gamma), determinant);
+
+        result[1] = field.div(field.mult(field.mult(beta, gamma), alpha) ^field.mult(field.mult(gamma, alpha), beta) ^field.mult(field.mult(delta, beta), delta) ^
+                                  field.mult(field.mult(beta, beta), beta) ^field.mult(field.mult(gamma, gamma), delta) ^field.mult(field.mult(delta, alpha), alpha), determinant);
+
+        result
+    }
+
+    pub struct AmES
+    {
+        _key: Key<Self>,
+
+        //_schedule: [u8; STATE_SIZE],
+
+        _generator: ChaChaRng,
+
+        _sbox: ArraySbox,
+
+        _field: PrimitivePolynomialField,
+
+        _polynomial: [u8; ROW_SIZE],
+        _permutation: [u8; STATE_SIZE],
+        _sequence: Vec<Action>,
+
+        _inv_polynomial: [u8; ROW_SIZE],
+        _inv_permutation: [u8; STATE_SIZE],
+        _rev_sequence: Vec<Action>
+    }
+
+    impl NewBlockCipher for AmES {
+        type KeySize = U32;
+
+        fn new(key: & Key<Self>) -> Self {
+
+            //This will become the field over which all cipher arithmatic is performed
+            let field = PrimitivePolynomialField::new(IrreducablePolynomial::Poly84320).expect("Not a primitive");
+
+            /* Copy the key */
+            let master_key = key.clone();
+
+            /* Setup the cryptographically secure reproducable random number generator */
+            let mut generator = ChaChaRng::from_seed(<<ChaChaRng as SeedableRng>::Seed>::from(master_key));
+
+            /* Get the number of 'rounds' used in the algorithm */
+            let round_count = generator.gen_range(10..=15);
+
+            /* Get the sequence of actions */
+            //Produce an AES like algorithm, with one Action after another. Action1, Action 2, Action3, Action 1, Action 2, ...
+            let mut sequence: Vec<_> = (0..round_count*ACTION_COUNT).into_iter().map(|x| Action::from_u32(x % ACTION_COUNT).unwrap()).collect();
+
+            sequence.shuffle(& mut generator);
+
+            /* Get the sbox */
+            let sbox = ArraySbox::new(
+                {
+                    let mut map: Vec<u8> = (0..=255).into_iter().collect();
+                    map.shuffle(&mut generator);
+                    map
+                }
+            );
+
+            /* Get the polynmial, ensuring the determinant of the circulant matrix generated from the polkynomial is non-zero */
+            let mut polynomial = [0; ROW_SIZE];
+
+            let uniform_byte = Uniform::from(0..=255);
+
+            //Keep generating random ppolynomials until we find one with a non-zero determinant
+            loop {
+                for coeff in polynomial.iter_mut() {
+                    *coeff = uniform_byte.sample(& mut generator);
+                }
+
+                if determinant(&field, &polynomial) != 0 {
+                    break;
+                }
+            }
+
+            /* Get the permutation vector */
+            let mut permutation = [0; STATE_SIZE];
+
+            for (i, element) in permutation.iter_mut().enumerate() {
+                *element = i as u8;
+            }
+
+            permutation.shuffle(& mut generator);
+
+            /* Calculate the inverse of all the actions */
+            let inverse_poly = invert_polynomial(&field, &polynomial);
+            let inverse_perm = invert_map(& permutation);
+            let reverse_sequ = sequence.iter().rev().map(|x| *x).collect();
+
+            AmES {
+                _key: master_key,
+
+                _generator: generator,
+
+                _sbox: sbox,
+
+                _field: field,
+
+                _polynomial: polynomial,
+                _permutation: permutation,
+                _sequence: sequence,
+
+                _inv_polynomial: inverse_poly,
+                _inv_permutation: inverse_perm,
+                _rev_sequence: reverse_sequ
+            }
+        }
+
+    }
+
+    impl BlockCipher for AmES {
+        type BlockSize = U16;
+        type ParBlocks = U1;
+
+        fn encrypt_block(&self, block: &mut Block<Self>) {
+            let mut state = State::new(&self._field, block.as_mut_slice());
+
+            state.transform(&self._sequence, self._key.as_slice(), &self._polynomial, &self._permutation, |x| self._sbox.forward(x));
+        }
+
+        fn decrypt_block(&self, block: &mut Block<Self>) {
+            let mut state = State::new(&self._field, block.as_mut_slice());
+
+            state.transform(&self._rev_sequence, self._key.as_slice(), &self._inv_polynomial, &self._inv_permutation, |x| self._sbox.reverse(x));
+        }
+
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,193 +1,63 @@
-use galois_2p8::{IrreducablePolynomial, PrimitivePolynomialField};
-use std::fmt;
-use galois_2p8::Field;
+mod ames_cipher;
+
+use crate::ames_cipher::ames::AmES;
+
+/*
+use cipher::generic_array::GenericArray;
+use cipher::consts::U16;
+use cipher::block::{BlockCipher, NewBlockCipher, Key};
 
 use std::time::{Instant};
+*/
 
-const STATE_SIZE: usize = 16;
-const ROW_SIZE: usize = 4;
+use block_modes::{BlockMode, Cbc};
+use block_modes::block_padding::Pkcs7;
 
-#[derive(Debug, Clone, Copy)]
-enum Action {
-    XOR,
-    Scramble,
-    Linear,
-    SBox
-}
+use hex_literal::hex;
 
-struct ArraySbox {
-    _forward: Vec<u8>,
-    _reverse: Vec<u8>
-}
-
-impl ArraySbox {
-    fn new(list: Vec<u8>) -> Self {
-        let mut reverse = vec![0; 256];
-
-        for (i, map_element) in list.iter().enumerate() {
-            reverse[*map_element as usize] = i as u8;
-        }
-
-        ArraySbox {
-            _forward: list,
-            _reverse: reverse
-        }
-    }
-
-    fn forward(&self, byte: u8) -> u8 {
-        self._forward[byte as usize]
-    }
-
-    fn reverse(&self, byte: u8) -> u8 {
-        self._reverse[byte as usize]
-    }
-
-}
-
-struct State<'a> {
-    _field: & 'a PrimitivePolynomialField,
-    _array: [u8; STATE_SIZE]
-}
-
-impl fmt::Debug for State<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("State")
-            .field("_array", &self._array)
-            .field("_field", &IrreducablePolynomial::Poly84310)
-            .finish()
-    }
-}
-
-impl<'a> State<'a> {
-    fn new(field: & 'a PrimitivePolynomialField, array: [u8; STATE_SIZE]) -> Self {
-        State {
-            _field: field,
-            _array: array
-        }
-    }
-
-    fn xor(& mut self, key: & [u8; STATE_SIZE]) {
-        for (state_byte, key_byte) in self._array.iter_mut().zip(key.iter()) {
-            *state_byte = self._field.add(*key_byte, *state_byte);
-        }
-    }
-
-    fn scramble(& mut self, permutation: & [u8; STATE_SIZE]) {
-        let mut tmp = [0; STATE_SIZE];
-
-        for (tmp_byte, permuter) in tmp.iter_mut().zip(permutation.iter()) {
-            *tmp_byte = self._array[*permuter as usize]
-        }
-
-        self._array = tmp;
-    }
-
-    fn linear(& mut self, polynomial: & [u8; ROW_SIZE]) {
-        for i in 0..ROW_SIZE {
-            let row = & mut self._array[i*ROW_SIZE..i*ROW_SIZE+ROW_SIZE];
-
-            let mut new_row = [0u8; ROW_SIZE];
-
-            for (j, new_row_element) in new_row.iter_mut().enumerate() {
-
-                for (k, row_element) in row.iter().enumerate() {
-                    //Perform some dirty modular arithmetic to shift each row
-                    let polynomial_element = polynomial[(k + 7 - j) % ROW_SIZE];
-                    //Perform matrix multiplication by multiplying each element and adding them up
-                    *new_row_element = self._field.add(*new_row_element, self._field.mult(polynomial_element, *row_element))
-                }
-            }
-
-            row.clone_from_slice(&new_row);
-        }
-    }
-
-    fn remap<F>(& mut self, sbox: &F)
-        where F: Fn(u8) -> u8
-    {
-        for byte in self._array.iter_mut() {
-            *byte = sbox(*byte)
-        }
-    }
-
-    fn transform<F>(& mut self,
-                 sequence: & [Action],
-                 key: & [u8; STATE_SIZE],
-                 polynomial: & [u8; ROW_SIZE],
-                 permutation: & [u8; STATE_SIZE],
-                 sbox: F)
-        where F: Fn(u8) -> u8
-    {
-
-        for action in sequence.iter() {
-            match action {
-                Action::XOR => {
-                    self.xor(key);
-                },
-                Action::Linear => {
-                    self.linear(polynomial);
-                },
-                Action::SBox => {
-                    self.remap(&sbox);
-                },
-                Action::Scramble => {
-                    self.scramble(permutation);
-                }
-            }
-        }
-    }
-
-}
-
-fn invert_map(map: & [u8]) -> [u8; STATE_SIZE] {
-    let mut inverted = [0; STATE_SIZE];
-
-    for (i, map_element) in map.iter().enumerate() {
-        inverted[*map_element as usize] = i as u8;
-    }
-
-    inverted
-}
+type AmESCbc = Cbc<AmES, Pkcs7>;
 
 fn main() {
 
-    let field = PrimitivePolynomialField::new(IrreducablePolynomial::Poly84320).expect("Not a primitive");
+    let key = hex!("91c3d48111ee4b2856eb31a6badb0a7f75a18dd26ba8ddaee03b6fe26343cb69");
+    let iv = hex!("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
+    let plaintext = b"444444444444444444444444444444444444444444444444444444444";
 
-    let data = [45, 38, 49, 76, 0, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+    println!("Plaintext: {}", std::str::from_utf8(plaintext).unwrap());
 
-    let key = [49, 34, 174, 243, 241, 148, 38, 43, 227, 59, 178, 170, 238, 125, 93, 157];
+    let cipher = AmESCbc::new_var(&key, &iv).unwrap();
+    let ciphertext = cipher.encrypt_vec(plaintext);
 
-    let mut state = State::new(&field, data);
+    println!("Encrypted: {:?}", ciphertext);
 
-    let sbox = ArraySbox::new(
-        vec![29, 45, 216, 58, 164, 57, 39, 12, 64, 21, 244, 3, 34, 232, 44, 190, 140, 137, 89, 32, 26, 170, 103, 62, 210, 9, 147, 214, 117, 194, 143, 197, 204, 22, 174, 220, 100, 255, 17, 200, 71, 14, 225, 139, 180, 234, 226, 0, 10, 125, 222, 122, 142, 78, 209, 199, 110, 238, 195, 132, 213, 115, 4, 85, 183, 182, 5, 86, 158, 254, 11, 151, 175, 191, 233, 179, 223, 97, 215, 119, 90, 186, 13, 235, 76, 161, 127, 250, 207, 178, 7, 219, 80, 145, 98, 55, 162, 196, 6, 144, 63, 167, 243, 113, 69, 172, 65, 75, 53, 101, 153, 94, 131, 46, 201, 92, 230, 79, 1, 245, 56, 43, 121, 169, 218, 123, 228, 72, 24, 41, 163, 141, 114, 173, 242, 165, 150, 18, 159, 42, 120, 60, 240, 99, 251, 52, 88, 146, 253, 192, 38, 23, 189, 82, 31, 33, 133, 16, 138, 176, 96, 187, 231, 155, 198, 205, 111, 149, 118, 47, 124, 177, 102, 83, 91, 30, 211, 148, 95, 221, 112, 48, 128, 130, 168, 236, 105, 193, 129, 136, 212, 249, 51, 67, 229, 104, 156, 25, 247, 37, 70, 126, 74, 108, 184, 109, 135, 154, 239, 116, 54, 66, 49, 203, 40, 20, 202, 35, 166, 208, 160, 36, 68, 217, 107, 87, 15, 77, 246, 61, 248, 157, 81, 252, 241, 171, 134, 188, 28, 227, 152, 185, 237, 93, 106, 8, 206, 2, 73, 224, 27, 50, 59, 84, 181, 19]
-    );
+    let cipher = AmESCbc::new_var(&key, &iv).unwrap();
+    let decrypted_ciphertext = cipher.decrypt_vec(&ciphertext).unwrap();
 
-    let permutation = [3, 4, 11, 9, 7, 0, 5, 15, 10, 6, 2, 13, 8, 12, 14, 1];
-    let invert_perm = invert_map(&permutation);
+    println!("Decrypted: {}", std::str::from_utf8(decrypted_ciphertext.as_slice()).unwrap());
 
-    let polynomial = [3, 1, 1, 2];
-    let inverse_poly = [11, 13, 9, 14];
+    /*
+    let data: [u8; 16] = [45, 38, 49, 76, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
 
-    let action_sequence = [Action::Linear, Action::Scramble, Action::SBox, Action::XOR, Action::Scramble, Action::SBox, Action::Scramble, Action::XOR, Action::SBox, Action::Linear];
-    let reverse_sequence: Vec<_> = action_sequence.iter().rev().map(|x| *x).collect();
+    let mut block = GenericArray::<u8, U16>::clone_from_slice(&data);
 
+    let ames = AmES::new(&Key::<AmES>::clone_from_slice(&key));
 
-    println!("State: {:?}", state);
+    println!("Before   : {:?}", block.as_slice());
 
     let start = Instant::now();
-    state.transform(&action_sequence, &key, &polynomial, &permutation, |x| sbox.forward(x));
+    ames.encrypt_block(& mut block);
     let duration = start.elapsed();
 
-    println!("State: {:?}", state);
+    println!("Encrypted: {:?}", block.as_slice());
 
-    state.transform(reverse_sequence.as_slice(), &key, &inverse_poly, &invert_perm, |x| sbox.reverse(x));
+    ames.decrypt_block(& mut block);
 
-    println!("State: {:?}", state);
+    println!("Decrypted: {:?}", block.as_slice());
 
+    println!("Encryption duration: {:?}", duration);
 
-    println!("Transform duration: {:?}", duration);
+    //println!("Hash: {}", scrypt_simple("hello bitch!", &ScryptParams::new(15, 18, 1)).expect("Bad scrypt"))
 
+*/
 
-    //println!("Compare: {}", state == data);
 }
